3aebf508ce4ca3b61946a483f86a14ef87326c1c
saeedjohar <saeed_johar@yahoo.com>
2018-10-14 23:53:20 +0300

    diceRoll with working gui"

--- src/week2/diceRoll.java
+++ src/week2/diceRoll.java
@@ -1,12 +1,15 @@
 package week2;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class diceRoll {
+public class diceRoll extends JFrame {
 
-	private JFrame frame;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -15,8 +18,8 @@ public class diceRoll {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					diceRoll window = new diceRoll();
-					window.frame.setVisible(true);
+					diceRoll frame = new diceRoll();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -25,19 +28,15 @@ public class diceRoll {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public diceRoll() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	}
 
 }
